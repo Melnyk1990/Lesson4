953c784fb284cbeacd05fcb968b4b199e0666b50
SERHII <krok.krg.ua.1990@gmail.com>
2023-09-10 03:06:32 +0300

    Addet task 4

--- main.py
+++ main.py
@@ -34,3 +34,17 @@ replacement = input('Enter a replacement word: ')
 
 new = userPush.replace(search, replacement)
 print('New line: ', new)
+
+print('*' * 10, 'TASK 4', 10 * '*')
+
+userPush = input('Enter line: ')
+
+print('1) Third character of the line: ', userPush[2])
+print('2) Penultimate character of the line: ', userPush[-2])
+print('3) First five (5) characters of the line: ', userPush[0:5])
+print('4) The entire line except the last two characters: ', userPush[0:-2])
+print('5) All symbols with paired indices: ', userPush[ : : 2])
+print('6) All characters with unpaired indices: ', userPush[1: :3])
+print('7) Print all characters in reverse order: ', userPush[: :-1])
+print('8) Print characters in reverse order one after another: ', userPush[ : :-2])
+print('9) Length of the eighth (8) line : ', len(userPush[ : :-2]))
